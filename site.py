dc2c6aa70b65dbdcaedac75a9eec39234d819317
Oscar Forner Martinez <violador.de.segmentos@gmail.com>
2017-01-11 22:15:09 +0000

    Basic stuff for me own website

---
+++ site.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask("forner")
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+if __name__ == "__main__":
+    app.run()
