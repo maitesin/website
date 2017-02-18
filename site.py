383b98a459fbf9ba8b407b480b65c6e71f438083
Oscar Forner Martinez <violador.de.segmentos@gmail.com>
2017-02-18 20:06:58 +0000

    Some changes to make the web nicer in the phones

--- site.py
+++ site.py
@@ -4,19 +4,19 @@ app = Flask("forner")
 
 @app.route("/")
 def index():
-    return render_template('home.html')
-
-@app.route("/blog")
-def blog():
     return render_template('blog.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
 
 @app.route("/projects")
 def projects():
     return render_template('projects.html')
+
+@app.route("/resume")
+def blog():
+    return render_template('resume.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
