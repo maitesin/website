e4eb83c247a82de280849c6801548f613d4a2a8c
Oscar Forner Martinez <violador.de.segmentos@gmail.com>
2017-03-05 09:20:14 +0000

    Add new views (currently empty) for the new templates

--- post/views.py
+++ post/views.py
@@ -7,6 +7,30 @@ def index(request):
     context = {}
     return render(request, 'blog.html', context)
 
+def year(request, year):
+    context = {}
+    return render(request, 'year.html', context)
+
+def month(request, year, month):
+    context = {}
+    return render(request, 'month.html', context)
+
+def day(request, year, month, day):
+    context = {}
+    return render(request, 'day.html', context)
+
+def post(request, year, month, day, title):
+    context = {}
+    return render(request, 'post.html', context)
+
+def category(request, name):
+    context = {}
+    return render(request, 'category.html', context)
+
+def tag(request, name):
+    context = {}
+    return render(request, 'tag.html', context)
+
 def projects(request):
     context = {}
     return render(request, 'projects.html', context)
