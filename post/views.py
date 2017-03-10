77e9a9d1d4e2c5d6fcd43daa00d1c935f269b58c
Oscar Forner Martinez <violador.de.segmentos@gmail.com>
2017-03-10 22:51:54 +0000

    Create class views using Generic DetailView and ListView from Django

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Tag, Post, Category, PostTag
 
@@ -10,32 +11,71 @@ class PostListView(ListView):
     def get_template_names(self):
         return ['blog.html']
 
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        context['title'] = self.get_page_title()
+        return context
+
+class IndexPostListView(PostListView):
+    def get_page_title(self):
+        return 'Latest posts'
+
     def get_queryset(self):
         return self.model.get_latest_posts(5)
 
-def year(request, year):
-    context = {}
-    return render(request, 'year.html', context)
+class YearPostListView(PostListView):
+    def get_page_title(self):
+        return 'Posts from %s' % self.kwargs['year']
 
-def month(request, year, month):
-    context = {}
-    return render(request, 'month.html', context)
+    def get_queryset(self):
+        return self.model.get_posts_from_year(self.kwargs['year'])
 
-def day(request, year, month, day):
-    context = {}
-    return render(request, 'day.html', context)
+class MonthPostListView(PostListView):
+    def get_page_title(self):
+        return 'Posts from %s %s' % (self.kwargs['month'], self.kwargs['year'])
 
-def post(request, year, month, day, title):
-    context = {}
-    return render(request, 'post.html', context)
+    def get_queryset(self):
+        return self.model.get_posts_from_year_month(self.kwargs['year'], self.kwargs['month'])
 
-def category(request, name):
-    context = {}
-    return render(request, 'category.html', context)
+class DayPostListView(PostListView):
+    def get_page_title(self):
+        return 'Posts from %s %s %s' % (self.kwargs['day'], self.kwargs['month'], self.kwargs['year'])
 
-def tag(request, name):
-    context = {}
-    return render(request, 'tag.html', context)
+    def get_queryset(self):
+        return self.model.get_posts_from_year_month_day(self.kwargs['year'], self.kwargs['month'], self.kwargs['day'])
+
+class CategoryPostListView(PostListView):
+    def get_page_title(self):
+        return 'Category %s' % Category.get_category_with_title(self.kwargs['name'])
+
+    def get_queryset(self):
+        return self.model.get_latest_posts_with_category(self.kwargs['name'])
+
+class TagPostListView(PostListView):
+    def get_page_title(self):
+        return 'Tag %s' % Tag.get_tag_with_title(self.kwargs['name'])
+
+    def get_queryset(self):
+        return self.model.get_latest_posts_with_tag(self.kwargs['name'])
+
+class AllPostListView(PostListView):
+    def get_page_title(self):
+        return 'All posts'
+
+    def get_queryset(self):
+        return self.model.get_latest_posts()
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_template_names(self):
+        return ['post.html']
+
+    def get_object(self):
+        for post in self.model.get_posts_from_year_month_day(self.kwargs['year'], self.kwargs['month'], self.kwargs['day']):
+            if post.get_title() == self.kwargs['title']:
+                return post
+        raise Http404("Post does not exist")
 
 def projects(request):
     context = {}
