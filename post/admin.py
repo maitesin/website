0057c695cb5b818f9b9f7970842380e4d0a8144c
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2019-03-10 16:32:56 +0000

    Remove old website and add hugo skeleton

--- post/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from post.models import Tag, Post, Category, PostTag
-
-admin.site.register(Tag)
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(PostTag)
